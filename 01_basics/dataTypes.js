aa73e700e51eedc92681b6d77feefd79e94e769d
vijaya0920 <58903091+vijaya0920@users.noreply.github.com>
2024-04-18 17:05:47 +0000

    data types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";       // treat all JS code below this stmt as a newer version of JS
+
+//alert(3+3)        we are using node js not browser . Thus stmt would work in browser 
+
+/* Learn about MDN documentaion and TC39 ECMA script  */
+
+let name = "Hitesh";    //string
+let age = 23;           //number
+let isLoggedIn = true;  //Boolean has true and false value
+let a;                  //undefined variable
+
+// null  => its a standalone value. 
+
+// undefined  => when we have not defined value but declared a variable
+
+// Object
+
+// print type of the variable
+console.log(typeof name);
+console.log(typeof age);
+console.log(isLoggedIn);
+console.log(typeof null);
+console.log(typeof a);
